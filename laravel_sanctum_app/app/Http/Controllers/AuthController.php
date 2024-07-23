46e176a10a18d917a2ddcde8bfdb24a9bfb127f1
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-07-23 12:40:18 +0530

    login and register created

---
+++ laravel_sanctum_app/app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create($data);
+
+        // $user = User::create([
+        //     'name' => $validatedData['name'],
+        //     'email' => $validatedData['email'],
+        //     'password' => Hash::make($validatedData['password']),
+        // ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+
+        // return response()->json([
+        //     'name' => $user->name,
+        //     'email' => $user->email,
+        // ]);
+    }
+
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'required|string|email|max:255|exists:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::where('email', $data['email'])->first();
+
+        if(!$user || !Hash::check($data['password'], $user->password)){
+            return response([
+                'message' => 'Bad Credentials',
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+}
